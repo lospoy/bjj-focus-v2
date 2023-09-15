1da6b655776e8cee412496d15ee02a3c02ae036c
Carlos <36555654+lospoy@users.noreply.github.com>
2023-09-15 10:39:54 -0400

    add intents router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { aimsRouter } from "./routers/aims";
+import { intentsRouter } from "./routers/intents";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { aimsRouter } from "./routers/aims";
  */
 export const appRouter = createTRPCRouter({
   aims: aimsRouter,
+  intents: intentsRouter,
 });
 
 // export type definition of API
