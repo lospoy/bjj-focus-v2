235f86d68d46c4c232c2c273b18c0800121ef7a7
Carlos <36555654+lospoy@users.noreply.github.com>
2023-09-21 16:20:30 -0400

    working shadcnui

--- src/components/intentWizard.tsx
+++ src/components/intentWizard.tsx
@@ -1,8 +1,23 @@
-import React, { useEffect } from "react";
-import { useForm, Controller } from "react-hook-form";
-import type { RouterOutputs } from "~/utils/api";
+import React from "react";
 import { api } from "~/utils/api";
+import Link from "next/link";
 
+// ShadCN UI Imports
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+
+// Types
+import type { RouterOutputs } from "~/utils/api";
+import { DatePickerWithRange } from "./ui/datepickerRange";
 type IntentData = RouterOutputs["intents"]["getById"];
 
 const useIntentData = (id: string): IntentData | undefined => {
@@ -11,95 +26,58 @@ const useIntentData = (id: string): IntentData | undefined => {
   return data;
 };
 
+const getIntentData = (id: string): IntentData | undefined => {
+  const { data } = api.intents.getById.useQuery({ id });
+
+  return data;
+};
+
+interface IntentFormData {
+  startDate: Date;
+  endDate: Date;
+  reminders: string;
+}
+
 interface IntentWizardProps {
   intentId?: string; // Pass intentId to edit an existing intent
   aimId?: string; // Pass aimId either to edit existing intent or as suggestion for new intent
 }
 
-export function IntentWizard({ intentId, aimId }: IntentWizardProps) {
-  const { control, handleSubmit, setValue } = useForm();
-  const onSubmit = (data: IntentData) => {
-    if (intentId) {
-      // Handle edit logic for an existing intent
-      console.log("edit intent", data);
-    } else {
-      // Handle save logic for a new intent
-      console.log("new intent", data);
-    }
-
-    // Other actions upon saving/editing
-  };
-
-  // Fetch existing intent data if intentId is provided
-  // useEffect(() => {
-  //   if (intentId) {
-  //     // Replace with your API request to fetch intent data by ID
-  //     useIntentData
-
-  //       // Populate form fields with existing data
-  //       Object.keys(existingIntentData).forEach((key) => {
-  //         setValue(key, existingIntentData[key]);
-  //       });
-  //     });
-  //   }
-  // }, [intentId, setValue]);
-
+export function IntentWizard() {
   return (
-    <div className="w-full rounded-lg bg-gray-100 p-4 shadow-md">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4">
-          {/* Add your form fields here */}
-          <Controller
-            name="startDate"
-            control={control}
-            render={({ field }) => (
-              <input
-                type="date"
-                {...field}
-                // Add defaultValue for initial values
-                defaultValue={aimId || ""}
-              />
-            )}
-          />
-        </div>
-        <div className="mb-4">
-          <Controller
-            name="endDate"
-            control={control}
-            render={({ field }) => (
-              <input
-                type="date"
-                {...field}
-                // Add defaultValue for initial values
-                defaultValue={aimId || ""}
-              />
-            )}
-          />
-        </div>
-        <div className="mb-4">
-          <Controller
-            name="reminders"
-            control={control}
-            render={({ field }) => (
-              <input
-                type="text"
-                {...field}
-                // Add defaultValue for initial values
-                defaultValue={aimId || ""}
-              />
-            )}
-          />
-        </div>
-        {/* Add other form fields */}
-        <div className="mt-4 flex justify-end">
-          <button
-            type="submit"
-            className="rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700"
-          >
-            Save
-          </button>
-        </div>
-      </form>
+    <div className="flex flex-col">
+      <div className="mt-10">
+        <Card className="mx-auto max-w-2xl">
+          <CardHeader>
+            <CardTitle>Create New Intent</CardTitle>
+            <CardDescription>
+              Enter details to create a new intent
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="dates">How long?</Label>
+                <DatePickerWithRange />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="email">Reminders</Label>
+                <Input
+                  id="email"
+                  placeholder="Reminders go here"
+                  required
+                  type="string"
+                />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full" type="submit">
+              Create Intent
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -52,9 +54,12 @@ module.exports = {
         },
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
         sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
@@ -73,4 +78,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
