6cdc08bd9f99218a5c3f7737504fe2a154c612f2
Carlos <36555654+lospoy@users.noreply.github.com>
2023-11-29 15:27:48 -0500

    add question mark

--- src/components/jitView.tsx
+++ src/components/jitView.tsx
@@ -17,14 +17,20 @@ import {
 } from "~/components/ui/card";
 import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
+import { QuestionMarkIcon } from "@radix-ui/react-icons";
 
 export const JitView = (props: { jit: Jit; isSelected: boolean }) => {
   const { jit } = props;
 
   return (
-    <Card key={jit.id} className="mb-8">
-      <div className=" flex h-[90px]">
-        <CardHeader className=" w-8/12 p-0 pl-6">
+    <Card key={jit.id} className="relative mb-9">
+      <div className="flex h-[90px]">
+        {/* Add the interrogation mark icon */}
+        <div className="absolute -left-2 -top-2 flex bg-white p-1">
+          <QuestionMarkIcon className="h-5 w-5" />
+          <QuestionMarkIcon className="-ml-2 h-4 w-4" />
+        </div>
+        <CardHeader className="w-8/12 p-0 pl-6">
           <div className="mt-3 flex h-full flex-col justify-center -space-y-1">
             <CardTitle className="-mt-2 text-2xl leading-5">
               {jit.name}
@@ -45,8 +51,8 @@ export const JitView = (props: { jit: Jit; isSelected: boolean }) => {
           </div>
         </CardContent>
       </div>
-      <CardFooter className="flex h-0 items-start justify-center ">
-        <Button className="mt-1 bg-accent font-mono font-semibold ">
+      <CardFooter className="flex h-0 items-start justify-center">
+        <Button className="mt-1 bg-accent font-mono font-semibold">
           ACTIVATE
         </Button>
       </CardFooter>
