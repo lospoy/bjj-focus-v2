fb26e8a945ea5c7455b713cd985fefb57f725d1b
Carlos <36555654+lospoy@users.noreply.github.com>
2023-12-15 00:46:09 -0500

    form sending correct mock JSON data

--- src/components/JitCreator.tsx
+++ src/components/JitCreator.tsx
@@ -1,18 +1,31 @@
-import { api } from "~/utils/api";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select";
-import { Button } from "./ui/button";
+"use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Check, ChevronsUpDown } from "lucide-react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
-import { toast } from "./ui/use-toast";
+import { api } from "~/utils/api";
+import { cn } from "~/libs/utils";
+import { Button } from "./ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "./ui/command";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { useToast } from "./ui/use-toast";
+import { useState } from "react";
 
 const FormSchema = z.object({
   move: z.string({
@@ -27,6 +40,7 @@ const FormSchema = z.object({
 });
 
 export const JitCreator = () => {
+  const { toast } = useToast();
   const allCategories = api.categories.getAll.useQuery().data;
   const allPositions = api.positions.getAll.useQuery().data;
   const allMoves = api.moves.getAll.useQuery().data;
@@ -37,17 +51,9 @@ export const JitCreator = () => {
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    try {
-      newJit.mutate({
-        categoryId: data.category,
-        moveId: data.move,
-        positionId: data.position,
-      });
-    } catch (e: unknown) {
-      toast({ title: "Failed to add session. Please try again later." });
-    }
+    console.log(data);
     toast({
-      title: "New Jit Created:",
+      title: "You submitted the following values:",
       description: (
         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
@@ -58,30 +64,63 @@ export const JitCreator = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        {/* MOVES */}
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <FormField
           control={form.control}
-          name="move"
+          name="category"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Select either a Move, a Position, or Both</FormLabel>
-              <Select onValueChange={field.onChange}>
-                <FormControl>
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Moves" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectGroup>
-                    {allMoves?.map((move) => (
-                      <SelectItem key={move.id} value={move.name}>
-                        {move.name}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+            <FormItem className="flex flex-col">
+              <FormLabel>Category</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className={cn(
+                        "w-[200px] justify-between",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value
+                        ? allCategories?.find(
+                            (category) => category.id === field.value,
+                          )?.name
+                        : "Select category"}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0">
+                  <Command>
+                    <CommandInput placeholder="Search category..." />
+                    <CommandEmpty>No category found.</CommandEmpty>
+                    <CommandGroup>
+                      {allCategories?.map((category) => (
+                        <CommandItem
+                          value={category.id}
+                          key={category.id}
+                          onSelect={() => {
+                            form.setValue("category", category.id);
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              category.name === field.value
+                                ? "opacity-100"
+                                : "opacity-0",
+                            )}
+                          />
+                          {category.name}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              <FormDescription>Yyeeeeeeah categories babeh.</FormDescription>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -90,55 +129,120 @@ export const JitCreator = () => {
           control={form.control}
           name="position"
           render={({ field }) => (
-            <FormItem>
-              <Select onValueChange={field.onChange}>
-                <FormControl>
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Positions" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectGroup>
-                    {allPositions?.map((position) => (
-                      <SelectItem key={position.id} value={position.name}>
-                        {position.name}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+            <FormItem className="flex flex-col">
+              <FormLabel>position</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className={cn(
+                        "w-[200px] justify-between",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value
+                        ? allPositions?.find(
+                            (position) => position.id === field.value,
+                          )?.name
+                        : "Select position"}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0">
+                  <Command>
+                    <CommandInput placeholder="Search position..." />
+                    <CommandEmpty>No position found.</CommandEmpty>
+                    <CommandGroup>
+                      {allPositions?.map((position) => (
+                        <CommandItem
+                          value={position.name}
+                          key={position.id}
+                          onSelect={() => {
+                            form.setValue("position", position.id);
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              position.name === field.value
+                                ? "opacity-100"
+                                : "opacity-0",
+                            )}
+                          />
+                          {position.name}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              <FormDescription>Yyeeeeeeah positions babeh.</FormDescription>
+              <FormMessage />
             </FormItem>
           )}
         />
-
         <FormField
           control={form.control}
-          name="category"
+          name="move"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                From that position and move combination, you are working on a...
-              </FormLabel>
-              <Select onValueChange={field.onChange}>
-                <FormControl>
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Categories" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectGroup>
-                    {allCategories?.map((category) => (
-                      <SelectItem key={category.id} value={category.name}>
-                        {category.name}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+            <FormItem className="flex flex-col">
+              <FormLabel>move</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className={cn(
+                        "w-[200px] justify-between",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value
+                        ? allMoves?.find((move) => move.id === field.value)
+                            ?.name
+                        : "Select move"}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0">
+                  <Command>
+                    <CommandInput placeholder="Search move..." />
+                    <CommandEmpty>No move found.</CommandEmpty>
+                    <CommandGroup>
+                      {allMoves?.map((move) => (
+                        <CommandItem
+                          value={move.name}
+                          key={move.id}
+                          onSelect={() => {
+                            form.setValue("move", move.id);
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              move.name === field.value
+                                ? "opacity-100"
+                                : "opacity-0",
+                            )}
+                          />
+                          {move.name}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              <FormDescription>Yyeeeeeeah moves babeh.</FormDescription>
+              <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">CREATE JIT</Button>
+        <Button type="submit">Submit</Button>
       </form>
     </Form>
   );
