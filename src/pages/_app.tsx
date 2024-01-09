6291ea47681d8874cbeee769a9db1be1b002c9f9
Carlos <36555654+lospoy@users.noreply.github.com>
2024-01-09 12:24:01 -0500

    delete redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,25 +2,21 @@ import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import { Provider } from "react-redux";
-import store from "~/store/store";
 import Head from "next/head";
 import TopNav from "~/components/TopNav";
 import { Toaster } from "~/components/ui/toaster";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <ClerkProvider>
-        <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#eb5683" />
-        </Head>
-        <TopNav />
-        <Component {...pageProps} />
-        <Toaster />
-      </ClerkProvider>
-    </Provider>
+    <ClerkProvider>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#eb5683" />
+      </Head>
+      <TopNav />
+      <Component {...pageProps} />
+      <Toaster />
+    </ClerkProvider>
   );
 };
 
